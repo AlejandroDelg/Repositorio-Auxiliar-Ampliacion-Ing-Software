f983d7d042db385e7a959b7ce7ea47842210dba9
AlejandroDelg <alexdelga2013@gmail.com>
2023-06-23 18:02:08 +0200

    Actualizado test selenium

--- src/test/java/es/codeurjc/ais/e2e/selenium/SeleniumTest.java
+++ src/test/java/es/codeurjc/ais/e2e/selenium/SeleniumTest.java
@@ -1,137 +1,66 @@
 package es.codeurjc.ais.e2e.selenium;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 import java.time.Duration;
-
-import org.openqa.selenium.NoSuchElementException;
-
-import org.junit.jupiter.api.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import es.codeurjc.ais.Application;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SeleniumTest 
-{
-    @LocalServerPort
-    int port;
+public class SeleniumTest {
 
-	private WebDriver driver;
+	@LocalServerPort
+	int port;
 
+	WebDriver driver;
+    private WebDriverWait wait;
+	
 	@BeforeAll
 	public static void setupClass() {
 		WebDriverManager.chromedriver().setup();
 	}
-
+	
 	@BeforeEach
-	public void setupTest() {
-		driver = new ChromeDriver();
+	public void setup() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+        //options.addArguments("no-sandbox");
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
-
+	
 	@AfterEach
 	public void teardown() {
-		if (driver != null) {
+		if(driver != null) {
 			driver.quit();
 		}
 	}
-	
-    @Test
-    public void theTwoTowers() throws InterruptedException 
-    {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-
-		driver.get("http://localhost:"+this.port+"/");
-        String mensaje = "The Lord of the Rings";
-        
-        wait.until(presenceOfElementLocated(By.name("topic")));
-        driver.findElement(By.name("topic")).sendKeys(mensaje);
-
-        wait.until(presenceOfElementLocated(By.id("search-button")));
-        driver.findElement(By.id("search-button")).click();
-
-        wait.until(presenceOfElementLocated(By.id("The Two Towers")));
-        driver.findElement(By.id("The Two Towers")).click();
-
-        wait.until(presenceOfElementLocated(By.id("bookTitle")));
-        String topic = driver.findElement(By.id("bookTitle")).getText();
-
-        assertEquals("The Two Towers",topic);
-        
-    }
     
     @Test
-    public void theReturnOfTheKingError()throws InterruptedException
-    {   
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		driver.get("http://localhost:"+this.port+"/");
-        String mensaje = "The Lord of the Rings";
-        wait.until(presenceOfElementLocated(By.name("topic")));
+    @DisplayName("Check that the default topic is fantasy")
+	public void checkDefaultTopic() throws Exception {
 
-        driver.findElement(By.name("topic")).sendKeys(mensaje);
-        wait.until(presenceOfElementLocated(By.id("search-button")));
-        driver.findElement(By.id("search-button")).click();
+        String topic = "fantasy";
 
-        wait.until(presenceOfElementLocated(By.id("The Return of the King")));
-        driver.findElement(By.id("The Return of the King")).click();
+        this.driver.get("http://localhost:"+this.port+"/");
+
+        String title = driver.findElement(By.tagName("h1")).getText();
         
-        wait.until(presenceOfElementLocated(By.id("error-message")));
-        String tituloRecibido = driver.findElement(By.id("error-message")).getText();
-        assertEquals("Error when retrieving a book: unsupported format",tituloRecibido);
-    	
+        assertEquals("Books (topic="+topic+")", title);
     }
-    
-    @Test
-    public void createReviewAndDelete() throws InterruptedException 
-    {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		driver.get("http://localhost:"+this.port+"/");
-        String mensaje = "epic fantasy";
-        wait.until(presenceOfElementLocated(By.name("topic")));
-        driver.findElement(By.name("topic")).sendKeys(mensaje);
-        
-        wait.until(presenceOfElementLocated(By.id("search-button")));
-        driver.findElement(By.id("search-button")).click();
-        
-        
-        wait.until(presenceOfElementLocated(By.id("A Game of Thrones")));
-        driver.findElement(By.id("A Game of Thrones")).click();
-
-        wait.until(presenceOfElementLocated(By.name("nickname")));
-        String titulo_review = "Esto es el titulo de la review";
-        driver.findElement(By.name("nickname")).sendKeys(titulo_review);
-
-        wait.until(presenceOfElementLocated(By.name("content")));
-        String descripcion_review = "Esto es la descripcion de la review";
-        driver.findElement(By.name("content")).sendKeys(descripcion_review);
-
-        
-        wait.until(presenceOfElementLocated(By.id("add-review")));
-        driver.findElement(By.id("add-review")).click();
-
-        
-        String idReview = titulo_review + "-delete";
-        wait.until(presenceOfElementLocated(By.id(idReview)));
-        driver.findElement(By.id(idReview)).click();
-        
-
-        assertThrows(NoSuchElementException.class, () -> {
-            driver.findElement(By.id(idReview)).getText();
-        });
-    }
-    
 }
