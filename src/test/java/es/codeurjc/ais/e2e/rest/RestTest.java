a8d869fb18ac439ead7a16f519baf2d76cab01d4
AlejandroDelg <alexdelga2013@gmail.com>
2023-06-28 14:52:47 +0200

    Actualizado sanity test

--- src/test/java/es/codeurjc/ais/e2e/rest/RestTest.java
+++ src/test/java/es/codeurjc/ais/e2e/rest/RestTest.java
@@ -2,31 +2,28 @@ package es.codeurjc.ais.e2e.rest;
 
 import static io.restassured.RestAssured.when;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("REST tests")
 public class RestTest {
-
     @LocalServerPort
     int port;
-
-    @BeforeEach
-    public void setUp() {
-        RestAssured.port = port;
-    }
-
     @Test
     @DisplayName("get All Books")
 	public void getAllBooks() throws Exception {
 
+        RestAssured.port = port;
         when()
             .get("/api/books/?topic=drama").
         then()
@@ -40,15 +37,20 @@ public class RestTest {
     @DisplayName("sanity test")
     public void sanityTest() throws Exception
     {
-        //espera de 30 segundos para que carge bien la aplicacion
-        Thread.sleep(10000);
         String host = System.getProperty("host");
-        RestAssured.baseURI = host;
-        Thread.sleep(10000);
-        when()
-        .get("/api/books/?topic=drama")
-        .then()
-        .statusCode(200);
+        Thread.sleep(1000);
+        // Realiza la solicitud REST utilizando Rest Assured
+        Response response = RestAssured.get(host + "/api/books/OL27479W");
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        String responseBody = response.getBody().asString();
+        String description = response.jsonPath().get("description");
+        int descriptionLength = description.length();
+        int maxLength = 953; // Longitud máxima esperada
+        System.out.println("descripcion de la respuesta: " + responseBody);
+        assertTrue("La descripción del libro es mayor a 953 caracteres.", descriptionLength <= maxLength);
+
     }
     
 }
